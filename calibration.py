d8ca008a29fec6a35a7388422bf64ecbebb72fe6
Elise Malmer Martinsen <elisemma@eduroam-193-157-215-106.wlan.uio.no>
2023-04-14 10:22:40 +0200

    Calibration functions for 18cm and 50cm are made. For 18cm we have that the calibration spectra for Eu152 don't look good (not a good peak fit) and the graph for the resolution calibration doesn't agree with the points in the same plot (the regression is off). For the 50cm calibration no peaks are found in the Eu spectrum. One peak is a bit off in the Ba spectrum.

--- calibration.py
+++ calibration.py
@@ -3,7 +3,6 @@ import curie as ci
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#/Users/elisemma/Library/CloudStorage/OneDrive-Personal/Dokumenter/Master/FilerFraAndrew/2017_Feb_Zr
 
 def calibration_18cm():
 
@@ -13,16 +12,6 @@ def calibration_18cm():
     sp_Ba133.isotopes = ['133BA'] #['133BA', '56CO', '137CS', '152EU']
     sp_Ba133.auto_calibrate()
     sp_Ba133.plot()
-
-    sp_Eu152 = ci.Spectrum('/Users/elisemma/Library/CloudStorage/OneDrive-Personal/Dokumenter/Master/FilerFraAndrew/2017_Feb_Zr/calibration/Eu152_150217_18cm.Spe')
-    sp_Eu152.isotopes = ['157EU'] 
-    sp_Eu152.auto_calibrate()
-    sp_Eu152.plot()
-
-    sp_newEu152 = ci.Spectrum('/Users/elisemma/Library/CloudStorage/OneDrive-Personal/Dokumenter/Master/FilerFraAndrew/2017_Feb_Zr/calibration/newEu152_030317_18cm.Spe')
-    sp_newEu152.isotopes = ['157EU'] 
-    sp_newEu152.auto_calibrate()
-    sp_newEu152.plot()
 
     sp_Cs137 = ci.Spectrum('/Users/elisemma/Library/CloudStorage/OneDrive-Personal/Dokumenter/Master/FilerFraAndrew/2017_Feb_Zr/calibration/Cs137_230317_18cm.Spe')
     sp_Cs137.isotopes = ['137CS'] 
@@ -43,22 +32,72 @@ def calibration_18cm():
     sp_Cs137_2.isotopes = ['137CS'] 
     sp_Cs137_2.auto_calibrate()
     sp_Cs137_2.plot()
+
+    sp_Eu152 = ci.Spectrum('/Users/elisemma/Library/CloudStorage/OneDrive-Personal/Dokumenter/Master/FilerFraAndrew/2017_Feb_Zr/calibration/Eu152_150217_18cm.Spe')
+    sp_Eu152.isotopes = ['157EU'] 
+    sp_Eu152.auto_calibrate()
+    sp_Eu152.plot()
+
+    sp_newEu152 = ci.Spectrum('/Users/elisemma/Library/CloudStorage/OneDrive-Personal/Dokumenter/Master/FilerFraAndrew/2017_Feb_Zr/calibration/newEu152_030317_18cm.Spe')
+    sp_newEu152.isotopes = ['157EU'] 
+    sp_newEu152.auto_calibrate()
+    sp_newEu152.plot()
     
-
-
-
 
     sources = [{'isotope':'133BA', 'A0':3.989E4, 'ref_date':'01/01/2009 12:00:00'},
                {'isotope':'137CS', 'A0':3.855E4, 'ref_date':'01/01/2009 12:00:00'},
                {'isotope':'152EU', 'A0':3.929E4, 'ref_date':'01/01/2009 12:00:00'}]
-    # sources = [{'isotope':'133BA', 'A0':3.989E4, 'ref_date':'01/01/2009 12:00:00'}]
 
     sources = pd.DataFrame(sources)
 
-    cb.calibrate([sp_Ba133, sp_Eu152, sp_newEu152, sp_Cs137, sp_newCs137, sp_Cs137_1, sp_Cs137_2], sources=sources)
+    cb.calibrate([sp_Ba133, sp_newCs137, sp_Cs137_1, sp_Cs137_2, sp_Eu152, sp_newEu152, sp_Cs137], sources=sources)
     cb.plot()
+
+
+
+def calibration_50cm():
+
+    cb = ci.Calibration()
+
+    sp_Ba133 = ci.Spectrum('/Users/elisemma/Library/CloudStorage/OneDrive-Personal/Dokumenter/Master/FilerFraAndrew/2017_Feb_Zr/calibration/Ba133_090317_50cm.Spe')
+    sp_Ba133.isotopes = ['133BA'] 
+    sp_Ba133.auto_calibrate()
+    sp_Ba133.plot()
+
+    sp_Cs137 = ci.Spectrum('/Users/elisemma/Library/CloudStorage/OneDrive-Personal/Dokumenter/Master/FilerFraAndrew/2017_Feb_Zr/calibration/Cs137_090317_50cm.Spe')
+    sp_Cs137.isotopes = ['137CS'] 
+    sp_Cs137.auto_calibrate()
+    sp_Cs137.plot()
+
+    sp_Eu152 = ci.Spectrum('/Users/elisemma/Library/CloudStorage/OneDrive-Personal/Dokumenter/Master/FilerFraAndrew/2017_Feb_Zr/calibration/Eu152_090317_50cm.Spe')
+    sp_Eu152.isotopes = ['137CS'] 
+    sp_Eu152.auto_calibrate()
+    sp_Eu152.plot()
+
+    sources = [{'isotope':'133BA', 'A0':3.989E4, 'ref_date':'01/01/2009 12:00:00'},
+               {'isotope':'137CS', 'A0':3.855E4, 'ref_date':'01/01/2009 12:00:00'},
+               {'isotope':'152EU', 'A0':3.929E4, 'ref_date':'01/01/2009 12:00:00'}]
+
+    sources = pd.DataFrame(sources)
+
+    cb.calibrate([sp_Ba133, sp_Cs137, sp_Eu152], sources=sources)
+    cb.plot()
+
 
 
 
 if __name__ == '__main__':
     calibration_18cm()
+    calibration_50cm()
+
+
+
+
+
+
+
+
+
+
+
+
