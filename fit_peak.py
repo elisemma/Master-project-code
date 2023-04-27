909eb5ac4f67f3b80040f9472f0d7e1379d3e8f0
Elise Malmer Martinsen <elisemma@eduroam-193-157-194-242.wlan.uio.no>
2023-04-27 17:09:36 +0200

    tried to understand the fit_peaks function

--- fit_peak.py
+++ fit_peak.py
@@ -7,24 +7,48 @@ import matplotlib.pyplot as plt
 
 def fit_peaks(spectrumName, position):
 
-    path = '/Users/elisemma/Library/CloudStorage/OneDrive-Personal/Dokumenter/Master/FilerFraAndrew/2017_Feb_Zr/30MeV/'
+    path = '/Users/elisemma/Library/CloudStorage/OneDrive-Personal/Dokumenter/Master/PlanBCode/2017_Feb_Zr/30MeV/'
     spectrumFile = path + spectrumName + '.Spe'
 
-    cb = ci.Calibration('/Users/elisemma/Library/CloudStorage/OneDrive-Personal/Dokumenter/Master/FilerFraAndrew/2017_Feb_Zr/ElisesGeneratedFiles/Calibration/calibration_' + position + '.json')
+    cb = ci.Calibration('/Users/elisemma/Library/CloudStorage/OneDrive-Personal/Dokumenter/Master/PlanBCode/MyGeneratedFiles/Calibration/json_files/calibration_' + position + '.json')
     # cb.plot()
 
     sp = ci.Spectrum(spectrumFile)
     sp.cb = cb 
 
-    effcal = list(cb.effcal)
-    cb.effcal = effcal
+    # sp.isotopes = ['89NB', '90NB', '91NB', '92NB', '93NB', '94NB', '95NB', '96NB', '97NB', '98NB', 
+    #                '90ZR', '91ZR', '92ZR', '93ZR', '94ZR', '95ZR', '96ZR', 
+    #                '86Y', '87Y', '88Y', '89Y', '90Y', '91Y', '92Y', '93Y', '94Y']
 
-    sp.isotopes = ['86Y', '87Y', '88Y', '88ZR', '87NB', '88NB', '89NB', '90NB', '91NB', '92NB','93NB', '94NB', '95NB', '96NB', '97NB', '98NB']
-    sp.fit_peaks(bg='constant')
-    sp._peaks = sp.peaks[(sp.peaks['counts']>300)]
-    sp._peaks = sp.peaks[(sp.peaks['chi2']<10)]
+    # sp.isotopes = ['89NB']
+
+    # peaks = {'energy': [511, 769.6], 'intensity': [162, 6.2], 'unc_intensity': [17, 0.6]}
+    # peaks = pd.DataFrame(peaks)
+
+    # peaks = {'energy': 1611, 'intensity': 100, 'unc_intensity': 10}
+    # # peaks = pd.DataFrame(peaks)
+
+
+    # # sp.fit_peaks(gammas = [peak1, peak2], bg='constant')
+    # # sp.fit_config = {'bg':'constant'}
+    # # sp.fit_config = {'bg': 'linear', 'gammas': peaks}
+    # sp.fit_peaks(gammas=[{'energy':1611, 'intensity':100, 'unc_intensity':10}])
+
+    # sp._peaks = sp.peaks[(sp.peaks['counts']>300)]
+    # sp._peaks = sp.peaks[(sp.peaks['chi2']<10)]
+    sp.isotopes = ['89NB']
+    sp.fit_peaks(gammas=[{'energy':1100.0, 'intensity':10.66, 'unc_intensity':0.55}]) #E=1100 works, E=1807 does not work
+    # sp.fit_peaks(gammas=ci.Isotope('40K').gammas(istp_col=True))
     sp.plot()
 
+
+    """
+    Eksempel:
+    sp.isotopes = ['152EU', '40K']
+    sp.fit_peaks(gammas=[{'energy':1460.8, 'intensity':10.66, 'unc_intensity':0.55}])
+    sp.fit_peaks(gammas=ci.Isotope('40K').gammas(istp_col=True))
+    sp.plot()
+    """
 
 
 
