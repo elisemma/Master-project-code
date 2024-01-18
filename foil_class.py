919d1652ae9979daff617a527ab7b060d96fb247
Elise Malmer Martinsen <elisemma@eduroam-193-157-206-14.wlan.uio.no>
2024-01-18 13:52:32 +0100

    I Have recalculeted the areal densities for the 30MeV stack. I have also made a file called foil_class.py which hopefully will give me the weighted average beam current of all the monitor reactions in one foil. This will then be used as input in the variance_min.py file which will do the variance minimization. This is not implemented yet. Kapton and silicone is added in the stack_calculations.py file

---
+++ foil_class.py
@@ -0,0 +1,161 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import curie as ci 
+import pandas as pd 
+from scipy.interpolate import splev, splrep
+
+#One file for running the stack calulations with varying dp values and save the csv files in a folder
+
+#This file with one class for foil and one function for variance minimization
+
+
+class Foil: 
+    #wclass hich returns the beam current with unc for one foil
+    def __init__(self, beam_energy_in_foil, target_material, reaction_list, A0_list, A0_unc_list, areal_dens, areal_dens_unc_percent):
+        self.beam_energy_in_foil = beam_energy_in_foil
+        self.target_material = target_material
+        self.reaction_list = reaction_list
+        self.A0_list = A0_list
+        self.A0_unc_list = A0_unc_list
+        self.areal_dens = areal_dens
+        self.areal_dens_unc_percent = areal_dens_unc_percent
+        self.decay_const_list = []
+        self.xs_mon_list = []
+        self.xs_mon_unc_list = [0,0,0]
+        self.beam_current_list = []
+        self.beam_current_unc_list = []
+        self.weighted_average_beam_current = None
+        self.var_weighted_average_beam_current = None
+
+
+    def assign_molar_mass(self):
+        # Assigning the molar mass of the target material
+        if self.target_material == 'Ni':
+            self.molar_mass  = 58.6934
+            self.molar_mass_unc = 0.0004
+
+        elif self.target_material == 'Ti':
+            self.molar_mass = 47.867
+            self.molar_mass_unc = 0.001
+
+        else:
+            print('Error: Unknown target material')
+
+
+    def calculate_decay_const(self):
+        # Calculating the decay constant of the production nuclei by checking the reaction happening 
+        for reaction_product in self.reaction_list:
+            if reaction_product == '46Sc':
+                self.decay_const_list.append(np.log(2)/(83.79*24*3600)) #1/[s]
+
+            elif reaction_product == '48V':
+                self.decay_const_list.append(np.log(2)/(15.9735*24*3600)) #1/[s]
+
+            elif reaction_product == '56Co':
+                self.decay_const_list.append(np.log(2)/(77.236*24*3600)) #1/[s])
+
+            elif reaction_product == '58Co':
+                self.decay_const_list.append(np.log(2)/(70.86*24*3600)) #1/[s])
+
+            elif reaction_product == '61Cu':
+                self.decay_const_list.append(np.log(2)/(3.339*3600)) #1/[s])
+
+            else:
+                print('Error: Unknown monitor reaction')
+
+
+
+    def monitor_cross_section(self):
+        # Using the IAEA data to get the monitor cross section for the energy in a foil
+        for reaction_product in self.reaction_list:
+            filename = 'IAEA_monitor_xs_' + reaction_product + '.txt'
+            with open(filename) as file:
+                lines = file.readlines()[7:-1]
+                E_list = []
+                xs_list = []
+                xs_unc_list = []
+
+                for line in lines:
+                    words = line.split()
+                    E_list.append(float(words[0]))
+                    xs_list.append(float(words[1]))
+                    xs_unc_list.append(float(words[2]))
+                file.close()
+
+            E = np.array(E_list)
+            xs = np.array(xs_list)
+            unc_xs = np.array(xs_unc_list)
+            weights = 1 / unc_xs
+            spl = splrep(E, xs, w=weights, k=4)
+
+            # Interpolate the entire list of energies
+            interpolated_xs = splev(self.beam_energy_in_foil, spl)
+
+            # Append the interpolated value to the xs_mon_list
+            self.xs_mon_list.append(interpolated_xs)
+            self.xs_mon_unc_list.append(np.interp(self.beam_energy_in_foil, E, unc_xs))  # Assuming linear interpolation for uncertainty
+
+    def calculate_beam_currents_w_unc(self, Energy, decay_const):
+        # Calculating the beam current with uncertainty by using the end of beam activity, areal density, molar mass, decay constant and monitor cross section
+        N_A = 6.0221408e+23
+        t_irr = 1200 #[s]
+        t_irr_unc = 3 #[s]
+        N_T = float(self.areal_dens)*N_A/self.molar_mass*10 #[nuclei/m^2] when areal_dens is given in mg/cm^2
+
+
+        for i in range(len(self.reaction_list)):
+            beam_current = self.A0/(N_T*self.xs_mon_list[i]*(1-np.exp(-self.decay_const_list*t_irr))) #[d/s]
+            beam_current_in_A = beam_current*1.60217634e-19 #[A]
+            beam_current_in_nA = beam_current_in_A*1e9 #[nA]
+
+
+            areal_dens_unc = self.areal_dens*10*self.areal_dens_unc_percent/100
+            N_T_unc = N_T*np.sqrt((areal_dens_unc/self.areal_dens)**2 + (self.molar_mass_unc/self.molar_mass)**2) #[nuclei/cm^2]
+        
+            dfdx_list = [] #Jacobian
+            unc_list = []
+        
+            dA0 = self.A0*1e-8
+            dfdA0 = (self.A0+dA0/(N_T*self.xs_mon_list[i]*(1-np.exp(-self.decay_const_list*t_irr))) - self.A0-dA0/(N_T*self.xs_mon_list[i]*(1-np.exp( -self.decay_const_list*t_irr))))/dA0
+            dfdx_list.append(dfdA0)
+            unc_list.append(self.A0_unc_list[i])
+        
+            # dN_T = N_T*1e-8
+            # dfdN_T = (self.A0/((N_T+dN_T)*self.xs_mon_list[i]*(1-np.exp(-self.decay_const_list*t_irr))) - self.A0/((N_T-dN_T)*self.xs_mon_list[i]*(1- np.exp(-self.decay_const_list*t_irr))))/dN_T
+            # dfdx_list.append(dfdN_T)
+            # unc_list.append(N_T_unc)
+        
+            dxs = self.xs_mon_list[i]*1e-8
+            dfdxs = (self.A0/(N_T*(self.xs_mon_list[i]+dxs)*(1-np.exp(-self.decay_const_list*t_irr))) - self.A0/(N_T*(self.xs_mon_list[i]-dxs)*(1-np. exp(-self.decay_const_list*t_irr))))/dxs
+            dfdx_list.append(dfdxs)
+            unc_list.append(self.xs_mon_unc_list[i])
+        
+            dt_irr = t_irr*1e-8
+            dfdt_irr = (self.A0/(N_T*self.xs_mon_list[i]*(1-np.exp(-self.decay_const_list*(t_irr+dt_irr)))) - self.A0/(N_T*self.xs_mon_list[i]*(1-np. exp(-self.decay_const_list*(t_irr-dt_irr)))))/dt_irr 
+            dfdx_list.append(dfdt_irr)
+            unc_list.append(t_irr_unc)
+        
+            dfdx = np.array(dfdx_list)
+            unc = np.array(unc_list)
+            beam_current_unc = np.sqrt(np.sum(np.multiply(dfdx,dfdx)* np.multiply(unc,unc)))*1. 60217634e-19*1e9
+
+            self.beam_current_list.append(beam_current_in_nA)
+            self.beam_current_unc_list.append(beam_current_unc)
+    
+
+
+    def calculate_weighted_average_beam_current(self):
+        # Calculating the weighted average beam current of all the monitor reactions happening in a specific foil
+        weight_list = []
+
+        for i in range(len(self.reaction_list)): 
+            weight = 1/self.beam_current_unc_list[i]**2
+            weight_list.append(weight)
+    
+        self.weighted_average_beam_current = np.sum(np.array(self.beam_current_list)*np.array(weight_list))/np.   sum(np.array(weight_list))
+
+        self.var_weighted_average_beam_current = np.sum((self.beam_current_list-self.weighted_average_beam_current)**2)/len(self.beam_current_list)
+        
+
+
+
--- stack_calculations.py
+++ stack_calculations.py
@@ -3,207 +3,124 @@ import matplotlib.pyplot as plt
 import curie as ci 
 import pandas as pd 
 from scipy.interpolate import splev, splrep
-# from statistics import variance
-# import os 
-# import glob
-# import seaborn as sns
 
 
+#print(ci.COMPOUND_LIST): ['RbCl', 'Inconel', 'H2O', 'SS_316', 'NaCl', 'Kapton', 'Acrylic', 'Air', 'Silicone', 'SrCO3', 'Polyester', 'Polyethylene', 'HDPE']
 
-stack_30MeV = [{'compound':'Fe', 'name': 'SS1', 'ad':100.199},#stainless steel compund ci.COMPOUND_LIST can be printed to get the stainless steel compound, add kapton and silicone  
-               {'compound':'Ni', 'name': 'Ni01', 'ad':23.253}, #recalc ad to check that they are correct 
+stack_30MeV = [{'compound':'SS_316', 'name': 'SS1', 'ad':100.199},
+
+               {'compound':'Kapton', 'name': 'Kapton1', 'ad':0.0036128},
+               {'compound':'Silicone', 'name': 'Silicone1', 'ad':0.0047952},
+               {'compound':'Ni', 'name': 'Ni01', 'ad':23.253}, 
+               {'compound':'Silicone', 'name': 'Silicone2', 'ad':0.0047952},
+               {'compound':'Kapton', 'name': 'Kapton2', 'ad':0.0036128},
+
+               {'compound':'Kapton', 'name': 'Kapton3', 'ad':0.0036128},
+               {'compound':'Silicone', 'name': 'Silicone3', 'ad':0.0047952},
                {'compound':'Zr', 'name': 'Zr01', 'ad':16.142},
+               {'compound':'Silicone', 'name': 'Silicone4', 'ad':0.0047952},
+               {'compound':'Kapton', 'name': 'Kapton4', 'ad':0.0036128},
+
+               {'compound':'Kapton', 'name': 'Kapton5', 'ad':0.0036128},
+               {'compound':'Silicone', 'name': 'Silicone5', 'ad':0.0047952},
                {'compound':'Ti', 'name': 'Ti01', 'ad':11.535},
+               {'compound':'Silicone', 'name': 'Silicone6', 'ad':0.0047952},
+               {'compound':'Kapton', 'name': 'Kapton6', 'ad':0.0036128},
+
                {'compound':'Al', 'name': 'E1', 'ad':68.312},
 
+
+
                {'compound':'Al', 'name': 'E2', 'ad':68.245},
+
+               {'compound':'Kapton', 'name': 'Kapton7', 'ad':0.0036128},
+               {'compound':'Silicone', 'name': 'Silicone7', 'ad':0.0047952},
                {'compound':'Ni', 'name': 'Ni02', 'ad':23.103},
+               {'compound':'Silicone', 'name': 'Silicone8', 'ad':0.0047952},
+               {'compound':'Kapton', 'name': 'Kapton8', 'ad':0.0036128},
+
+               {'compound':'Kapton', 'name': 'Kapton9', 'ad':0.0036128},
+               {'compound':'Silicone', 'name': 'Silicone9', 'ad':0.0047952},
                {'compound':'Zr', 'name': 'Zr02', 'ad':16.378},
-               {'compound':'Ti', 'name': 'Ti02', 'ad':11.739},
+               {'compound':'Silicone', 'name': 'Silicone10', 'ad':0.0047952},
+               {'compound':'Kapton', 'name': 'Kapton10', 'ad':0.0036128},
+
+               {'compound':'Kapton', 'name': 'Kapton11', 'ad':0.0036128},
+               {'compound':'Silicone', 'name': 'Silicone11', 'ad':0.0047952},
+               {'compound':'Ti', 'name': 'Ti02', 'ad':11.740},
+               {'compound':'Silicone', 'name': 'Silicone12', 'ad':0.0047952},
+               {'compound':'Kapton', 'name': 'Kapton12', 'ad':0.0036128},
+
+
 
                {'compound':'Al', 'name': 'E3', 'ad':68.349},
+
+               {'compound':'Kapton', 'name': 'Kapton13', 'ad':0.0036128},
+               {'compound':'Silicone', 'name': 'Silicone13', 'ad':0.0047952},
                {'compound':'Ni', 'name': 'Ni03', 'ad':23.064},
-               {'compound':'Zr', 'name': 'Zr03', 'ad':16.141},
+               {'compound':'Silicone', 'name': 'Silicone14', 'ad':0.0047952},
+               {'compound':'Kapton', 'name': 'Kapton14', 'ad':0.0036128},
+
+               {'compound':'Kapton', 'name': 'Kapton15', 'ad':0.0036128},
+               {'compound':'Silicone', 'name': 'Silicone15', 'ad':0.0047952},
+               {'compound':'Zr', 'name': 'Zr03', 'ad':16.140},
+               {'compound':'Silicone', 'name': 'Silicone16', 'ad':0.0047952},
+               {'compound':'Kapton', 'name': 'Kapton16', 'ad':0.0036128},
+
+               {'compound':'Kapton', 'name': 'Kapton17', 'ad':0.0036128},
+               {'compound':'Silicone', 'name': 'Silicone17', 'ad':0.0047952},
                {'compound':'Ti', 'name': 'Ti03', 'ad':11.273},
+               {'compound':'Silicone', 'name': 'Silicone18', 'ad':0.0047952},
+               {'compound':'Kapton', 'name': 'Kapton18', 'ad':0.0036128},
 
+
+
+               {'compound':'Kapton', 'name': 'Kapton19', 'ad':0.0036128},
+               {'compound':'Silicone', 'name': 'Silicone19', 'ad':0.0047952},
                {'compound':'Ni', 'name': 'Ni04', 'ad':23.201},
+               {'compound':'Silicone', 'name': 'Silicone20', 'ad':0.0047952},
+               {'compound':'Kapton', 'name': 'Kapton20', 'ad':0.0036128},
+
+               {'compound':'Kapton', 'name': 'Kapton21', 'ad':0.0036128},
+               {'compound':'Silicone', 'name': 'Silicone21', 'ad':0.0047952},
                {'compound':'Zr', 'name': 'Zr04', 'ad':16.195},
-               {'compound':'Ti', 'name': 'Ti04', 'ad':11.098},
+               {'compound':'Silicone', 'name': 'Silicone22', 'ad':0.0047952},
+               {'compound':'Kapton', 'name': 'Kapton22', 'ad':0.0036128},
 
+               {'compound':'Kapton', 'name': 'Kapton23', 'ad':0.0036128},
+               {'compound':'Silicone', 'name': 'Silicone23', 'ad':0.0047952},
+               {'compound':'Ti', 'name': 'Ti04', 'ad':11.099},
+               {'compound':'Silicone', 'name': 'Silicone24', 'ad':0.0047952},
+               {'compound':'Kapton', 'name': 'Kapton24', 'ad':0.0036128},
+
+
+
+               {'compound':'Kapton', 'name': 'Kapton25', 'ad':0.0036128},
+               {'compound':'Silicone', 'name': 'Silicone25', 'ad':0.0047952},
                {'compound':'Ni', 'name': 'Ni05', 'ad':22.746},
+               {'compound':'Silicone', 'name': 'Silicone26', 'ad':0.0047952},
+               {'compound':'Kapton', 'name': 'Kapton26', 'ad':0.0036128},
+
+               {'compound':'Kapton', 'name': 'Kapton27', 'ad':0.0036128},
+               {'compound':'Silicone', 'name': 'Silicone27', 'ad':0.0047952},
                {'compound':'Zr', 'name': 'Zr05', 'ad':16.400},
+               {'compound':'Silicone', 'name': 'Silicone28', 'ad':0.0047952},
+               {'compound':'Kapton', 'name': 'Kapton28', 'ad':0.0036128},
+
+               {'compound':'Kapton', 'name': 'Kapton29', 'ad':0.0036128},
+               {'compound':'Silicone', 'name': 'Silicone29', 'ad':0.0047952},
                {'compound':'Ti', 'name': 'Ti05', 'ad':11.317},
-               {'compound':'Fe', 'name': 'SS2', 'ad':100.865}]
+               {'compound':'Silicone', 'name': 'Silicone30', 'ad':0.0047952},
+               {'compound':'Kapton', 'name': 'Kapton30', 'ad':0.0036128},
 
-
-st = ci.Stack(stack_30MeV, E0=30, dE0 = 0.3, N=1000000, particle='d', dp = 1)
-# st.plot()
-# print(st.stack)
-
-
-
-def get_xs_from_exfor_files(filename):
-    with open(filename) as file:
-        lines = file.readlines()[7:-1]
-        E_list = []
-        xs_list = []
-        xs_unc_list = []
-
-        for line in lines:
-            words = line.split()
-            E_list.append(float(words[0]))
-            xs_list.append(float(words[1]))
-            xs_unc_list.append(float(words[2]))
-        file.close()
-
-    # return np.array(E_list), np.array(E_unc_list), np.array(xs_list), np.array(xs_unc_list)
-    return E_list, xs_list, xs_unc_list
-
-
-# def interpol_xs_unc(xs_unc_list, E_xs_list):
-
-#     E_xs_list, index = np.unique(np.array(E_xs_list), return_index = True)
-#     xs_unc_list = np.array(xs_unc_list)
-#     xs_unc_list = xs_unc_list[index]
-
-#     cs = CubicSpline(E_xs_list, xs_unc_list)
-#     # cs = interp1d(E_xs_list, xs_unc_list, kind = 'nearest')
-#     # plt.plot(E_xs_list, cs(E_xs_list))
-#     # plt.xlabel('Energy (MeV)')
-#     # plt.ylabel('xs (mb)')
-#     # plt.show()
-#     return cs
-
-
-def interpol_xs_unc(Energies, cross_sections, unc_cross_sections):
-    E = np.array(Energies)
-    xs = np.array(cross_sections)
-    unc_xs = np.array(unc_cross_sections)
-
-    weights = 1/unc_xs
-
-    spl = splrep(E, xs, w=weights, k=4)
-
-    return spl
+               {'compound':'SS_316', 'name': 'SS2', 'ad':100.865}]
 
 
 
-def convert_A0_to_beam_current_w_unc(A0, A0_unc, areal_dens, areal_dens_unc_percent, Mm, Mm_unc, decay_const, xs_mon, xs_mon_unc):
-    N_A = 6.0221408e+23
-    t_irr = 1200 #[s]
-    t_irr_unc = 3 #[s]
-    N_T = float(areal_dens)*N_A/Mm*10 #[nuclei/m^2] when areal_dens is given in mg/cm^2
-
-    beam_current = A0/(N_T*xs_mon*(1-np.exp(-decay_const*t_irr))) #[d/s]
-    beam_current_in_A = beam_current*1.60217634e-19 #[A]
-    beam_current_in_nA = beam_current_in_A*1e9 #[nA]
-
-    areal_dens_unc = areal_dens*10*areal_dens_unc_percent/100
-    N_T_unc = N_T*np.sqrt((areal_dens_unc/areal_dens)**2 + (Mm_unc/Mm)**2) #[nuclei/cm^2]
-
-    dfdx_list = [] #Jacobian
-    unc_list = []
-
-    dA0 = A0*1e-8
-    dfdA0 = (A0+dA0/(N_T*xs_mon*(1-np.exp(-decay_const*t_irr))) - A0-dA0/(N_T*xs_mon*(1-np.exp(-decay_const*t_irr))))/dA0
-    dfdx_list.append(dfdA0)
-    unc_list.append(A0_unc)
-
-    dN_T = N_T*1e-8
-    dfdN_T = (A0/((N_T+dN_T)*xs_mon*(1-np.exp(-decay_const*t_irr))) - A0/((N_T-dN_T)*xs_mon*(1-np.exp(-decay_const*t_irr))))/dN_T
-    dfdx_list.append(dfdN_T)
-    unc_list.append(N_T_unc)
-
-    dxs = xs_mon*1e-8
-    dfdxs = (A0/(N_T*(xs_mon+dxs)*(1-np.exp(-decay_const*t_irr))) - A0/(N_T*(xs_mon-dxs)*(1-np.exp(-decay_const*t_irr))))/dxs
-    dfdx_list.append(dfdxs)
-    unc_list.append(xs_mon_unc)
-
-    dt_irr = t_irr*1e-8
-    dfdt_irr = (A0/(N_T*xs_mon*(1-np.exp(-decay_const*(t_irr+dt_irr)))) - A0/(N_T*xs_mon*(1-np.exp(-decay_const*(t_irr-dt_irr)))))/dt_irr 
-    dfdx_list.append(dfdt_irr)
-    unc_list.append(t_irr_unc)
-
-    dfdx = np.array(dfdx_list)
-    unc = np.array(unc_list)
-    beam_current_unc = np.sqrt(np.sum(np.multiply(dfdx,dfdx)* np.multiply(unc,unc)))*1.60217634e-19*1e9
-
-    return beam_current_in_nA, beam_current_unc
+st = ci.Stack(stack_30MeV, E0=30, dE0 = 0.3, N=100000, particle='d', dp = 1)
+st.plot()
 
 
 
-def weighted_average_beam_current(reaction_list, A0_list, A0_unc_list, areal_dens_list,areal_dens_unc_list,  Mm_list, Mm_unc_list, decay_const_list, xs_mon_list, xs_mon_unc_list):
-    beam_current_list = []
-    beam_current_unc_list = []
-    weight_list = []
-
-    for i in range(len(reaction_list)):
-        beam_current, beam_current_unc = convert_A0_to_beam_current_w_unc(A0_list[i], areal_dens_list[i], Mm_list[i],    
-        decay_const_list[i], xs_mon_list[i], A0_unc_list[i], areal_dens_unc_list[i], Mm_unc_list[i], xs_mon_unc_list[i])
-        beam_current_list.append(beam_current)
-        beam_current_unc_list.append(beam_current_unc)
-        weight = 1/beam_current_unc**2
-        weight_list.append(weight)
-        print(f'Beam current for {reaction_list[i]}: {beam_current:.3f} +- {beam_current_unc:.3f}nA')
-        print(f'cross section for {reaction_list[i]}: {xs_mon_list[i]}\n')
-
-
-    weighted_average_beam_cur = np.sum(np.array(beam_current_list)*np.array(weight_list))/np.sum(np.array(weight_list))
-    var_weighted_average_beam_cur = np.sum((beam_current_list-weighted_average_beam_cur)**2)/len(beam_current_list)
-
-    chi_squared = chi_squared_func(beam_current_list, beam_current_unc_list, weighted_average_beam_cur)
-
-    print(f'Weighted average beam current: {weighted_average_beam_cur:.3f} +- {np.sqrt(var_weighted_average_beam_cur):.3f} nA')
-
-    return weighted_average_beam_cur, var_weighted_average_beam_cur, chi_squared, beam_current_list, beam_current_unc_list
-
-
-
-
-def chi_squared_func(x_list, x_unc_list, x_true):
-
-    if (type(x_true)==float):
-        x_true_array = np.zeros(len(x_list))
-        x_true_array.fill(xs_true)
-    else:
-        x_true_array = np.array(x_true)
-
-    x_array = np.array(x_list)
-    x_unc_array = np.array(x_unc_list)
-    x_diff = x_true_array-x_array
-
-    chi_squared = np.sum( np.multiply(x_diff, x_diff)/ np.multiply(x_unc_array, x_unc_array))
-    print('Nå er vi inni chi_squared_func________________________________________________________________')
-    print(f'x_unc_array = {x_unc_array}')
-    print(f'x_array = {x_array}')
-    print(f'x_true_array = {x_true_array}')
-    print(f'x_diff = {x_diff}')
-    print(f'chi squared = {chi_squared}')
-    print('______________________________________________________________________________________________')
-
-    return chi_squared
-
-
-
-# tregner en func til å variere E og dp for å finne optimal beam cur.
-# def finding_minimal_chi2():
-    #må tenke på dette
-
-
-
-
-#_____________________________________________Running the code____________________________________________________________
-
-# file = './Monitor_cross_section_data/natNi_dx_61Cu.txt'
-file = './Monitor_cross_section_data/natTi_dx_46Sc.txt'
-
-E_list, xs_list, xs_unc_list = get_xs_from_exfor_files(file)
-xs_interpol = interpol_xs_unc(E_list, xs_list, xs_unc_list)
-E_array = np.linspace(0,80,1000000)
-
-# plt.plot(E_array, xs_interpol(E_array))
-# plt.plot(E_array, splev(E_array, xs_interpol))
-# plt.plot(E_list, xs_list)
-plt.show()
 
 
